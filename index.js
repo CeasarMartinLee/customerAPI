15ffa54688ffc75b5407484792d2e4668236fbe2
“Ceasar” <lee_ceasar@yahoo.com>
2019-02-06 10:27:47 +0100

    deploy to heroku

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const customersRouter = require('./customers/routes')
 const companiesRouter = require('./companies/routes')
-
+const loginsRouter = require('./auth/routes')
+const usersRouter = require('./users/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -11,5 +12,7 @@ app
   .use(bodyParser.json())
   .use(customersRouter)
   .use(companiesRouter)
+  .use(loginsRouter)
+  .use(usersRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
 
